b822d1450111ffa793dcce18415b9e16371599ab
gaurav.bagga <gaurav.bagga@g.com>
2015-02-27 00:19:44 +0530

    json jackson annotation added for json marshalling

--- src/main/java/com/green/jwt/model/User.java
+++ src/main/java/com/green/jwt/model/User.java
@@ -1,15 +1,23 @@
 package com.green.jwt.model;
 
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonProperty;
+
 public class User {
 	
 	public final String name;
 	public final String password;
 	public final Role role;
 	
-	public User(String name, String password,Role role){
+    @JsonCreator
+	public User(@JsonProperty("name") String name,
+			@JsonProperty("password") String password,
+			@JsonProperty("role") Role role){
 		this.name = name;
 		this.password = password;
 		this.role = role;
 	}
+        
+        
 
 }
