7e191e1aa3b4a174403414c3f8be32b57c0e9490
gaurav.bagga <gaurav.bagga@g.com>
2015-02-27 00:19:12 +0530

    book entity added

---
+++ src/main/java/com/green/jwt/model/Book.java
@@ -0,0 +1,30 @@
+package com.green.jwt.model;
+
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+public class Book {
+
+	public final Long id;
+	public final String isbn;
+	public final String title;
+	public final String author;
+	public final Integer cost;//cents
+	
+	@JsonCreator
+	public Book(@JsonProperty("id") Long id, 
+			@JsonProperty("isbn") String isbn, 
+			@JsonProperty("title") String title, 
+			@JsonProperty("author") String author, 
+			@JsonProperty("cost") Integer cost) {
+		super();
+		this.id = id;
+		this.isbn = isbn;
+		this.title = title;
+		this.author = author;
+		this.cost = cost;
+	}
+	
+	
+	
+}
